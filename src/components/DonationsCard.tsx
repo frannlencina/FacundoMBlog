86c8e592fa294e8b55a4b17042add50dd60ecadb
Francisco <cinkii@protonmail.com>
2024-07-25 22:58:23 -0300

    feat ✨: mp external donation link

--- src/components/DonationsCard.tsx
+++ src/components/DonationsCard.tsx
@@ -1,8 +1,10 @@
 import { Toaster, toast } from 'sonner'
-
+import Link from 'next/link';
 export default function DonationCard() {
 
     const MP_ACC_ID = process.env.NEXT_PUBLIC_MP_ACC_ID
+    const MP_ACC_LINK = process.env.NEXT_PUBLIC_MP_ACC_LINK
+
     const copyText = () => {
         if (MP_ACC_ID) {
             navigator.clipboard.writeText(MP_ACC_ID);
@@ -19,7 +21,7 @@ export default function DonationCard() {
                     <button onClick={copyText} className="px-2.5 py-2 bg-[--mid-blue] text-white rounded-full hover:bg-[--light-blue] hover:text-[--mid-blue] cursor-pointer "><i className="ri-file-copy-line"></i></button>
                     <p className="inline-block ml-2 opacity-50  max-[252px]:max-w-[50px] max-w-[150px] truncate">{MP_ACC_ID ? MP_ACC_ID : 'Cargando...' }</p>
                 </div>
-                <span className="relative -top-1 mr-2 font-bold text-[--grey]">...</span>
+                <Link href={MP_ACC_LINK ? MP_ACC_LINK : ''} target='_blank' className="relative mr-3 font-bold text-[--grey]"><svg xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-link"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg></Link>
             </div>
             <div className="flex flex-col gap-2 justify-start mt-5">
                 <h5 className="text-[--light-grey] opacity-70">Balance actual</h5>
